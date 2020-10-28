29989dd1eecf685192ea4300687b05fe652c3c90
SumitKush1819 <43988713+SumitKush1819@users.noreply.github.com>
2020-10-28 22:58:21 +0530

    Add files via upload

---
+++ Face Detection/cv_video_read.py
@@ -0,0 +1,25 @@
+# Read a Video Stream from Camera(Frame by Frame)
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret,frame = cap.read()
+	gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+	if ret == False:
+		continue
+
+
+	cv2.imshow("Video Frame",frame)
+	#For gray image
+	cv2.imshow("Gray Frame",gray_frame)
+
+	#Wait for user input - q, then you will stop the loop
+	key_pressed = cv2.waitKey(1) & 0xFF
+	if key_pressed == ord('q'):
+		break
+
+
+cap.release()
+cv2.destroyAllWindows()
